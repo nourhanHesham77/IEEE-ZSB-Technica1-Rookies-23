15dc5267930ede39d87c8cbb53c5b85cc56916eb
Nourhan Hesham <117953315+nourhanHesham77@users.noreply.github.com>
2022-12-10 23:15:26 +0200

    Update problem8.js

--- Task-1/problem8.js
+++ Task-1/problem8.js
@@ -1,7 +1,73 @@
 //linked list.....
 
-class LinkedList {
-    constructor(head = null) {
-        this.head = head
-    }
+class Node
+{
+  constructor(data)
+  {
+    this.data=data;
+    this.next=null;
+  }
 }
+
+class LinkedList
+{
+  constructor()
+  {
+    this.head=null;
+    this.size=0;
+  }
+//insertion.....
+  insert(data)
+  {
+    let node = new Node(data); 
+    if(this.head==null)
+    {
+      this.head = node;
+      this.size++;
+    }else{
+      let current = this.head;
+      while(current.next!=null){
+        current = current.next;
+      }
+      current.next = node;
+      this.size++;
+    }
+  }
+
+  //deletion....
+  remove(data){
+   let current = this.head;
+   let prev = null;
+   while(current!=null){
+           if(current.data==data){
+            if(prev==null){
+              //which means w r del. first node..
+              this.head=current.next;
+            }else{
+              prev.next=current.next;
+            }
+            this.size--;
+            return current.data;
+           }
+           prev=current;
+           current=current.next;
+   }
+   return -1;
+  }
+
+  printList()
+  {
+      while(this.head != null){
+        console.log(this.head.data);
+        this.head=this.head.next;
+      }
+  }
+}
+
+var llist = new LinkedList();
+llist.insert(5);
+llist.insert(7);
+llist.insert(8);
+llist.insert(13);
+
+llist.printList();
